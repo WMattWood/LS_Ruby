6aa678684ca65dade2151ecc01efc218f44a7544
Matt Wood <w.matthew.wood@gmail.com>
2021-06-10 16:09:15 +0000

    Committing files which were deleted (i think there might be a better way to handle these leftover 'deleted' files when I change folder names)

--- intro_to_programming_with_ruby/basics_movies.rb
+++
@@ -1,5 +0,0 @@
-movies = { "Batman" => 1989, "Jaws" => 1976, "Star Wars" => 1976, "2001: A Space Oddyssey" => 1970 }
-puts movies["Batman"]
-puts movies["Jaws"]
-puts movies["Star Wars"]
-puts movies["2001: A Space Oddyssey"]
