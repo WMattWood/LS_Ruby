61122db149121417460b27f20be8fdaa9e9ccbf8
Matt Wood <w.matthew.wood@gmail.com>
2021-08-09 21:22:13 -0400

    Adding division.rb

---
+++ ruby_basics/user_input/division.rb
@@ -0,0 +1,21 @@
+def valid_number?(number_string)
+  number_string.to_i.to_s == number_string
+end
+
+loop do
+  puts ">> Please enter the numerator"
+  numerator = gets.chomp
+  break if valid_number?(numerator)
+  puts "Invalid input. Only integers are allowed."
+end
+
+loop do
+  puts ">> Please enter the denominator"
+  denominator = gets.chomp
+  break if valid_number?(denominator))
+  puts "Invalid input. Only integers are allowed."
+end
+
+result = numerator / denominator
+
+puts "#{numerator} / #{denominator} = #{result}"
