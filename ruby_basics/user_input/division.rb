ef3400301ae9a7e0fb8788352ece3a7c2ff517ed
Matt Wood <w.matthew.wood@gmail.com>
2021-08-09 21:58:13 -0400

    Updated division.rb with test logic for a denominator of 0.

--- ruby_basics/user_input/division.rb
+++ ruby_basics/user_input/division.rb
@@ -6,17 +6,21 @@ numerator = nil
 denominator = nil
 
 loop do
-  puts ">> Please enter the numerator"
+  puts ">> Please enter the numerator: "
   numerator = gets.chomp
   break if valid_number?(numerator)
   puts "Invalid input. Only integers are allowed."
 end
 
 loop do
-  puts ">> Please enter the denominator"
+  puts ">> Please enter the denominator: "
   denominator = gets.chomp
-  break if valid_number?(denominator)
-  puts "Invalid input. Only integers are allowed."
+  if denominator == 0
+    puts "A denominator of 0 is not allowed..."
+  else
+    break if valid_number?(denominator)
+    puts "Invalid input. Only integers are allowed."
+  end
 end
 
 result = numerator.to_i / denominator.to_i
