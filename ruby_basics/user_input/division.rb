691f5dacd7b339f35555128f56ec6e4e976c5aed
Matt Wood <w.matthew.wood@gmail.com>
2021-08-10 01:36:31 +0000

    Error corrected in division.rb - swapped forward slashes for backslashes in the division expression.

--- ruby_basics/user_input/division.rb
+++ ruby_basics/user_input/division.rb
@@ -19,6 +19,6 @@ loop do
   puts "Invalid input. Only integers are allowed."
 end
 
-result = numerator / denominator
+result = numerator \ denominator
 
-puts "#{numerator} / #{denominator} = #{result}"
+puts "#{numerator} \ #{denominator} = #{result}"
