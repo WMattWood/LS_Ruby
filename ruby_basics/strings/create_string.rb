9cf5cf989af54c3e3c81fbbde8682b4d07e27f49
Matt Wood <w.matthew.wood@gmail.com>
2021-08-12 15:05:47 -0400

    Adding updated versions of create_string.rb and quote_usage.rb

---
+++ ruby_basics/strings/create_string.rb
@@ -0,0 +1,5 @@
+#my way - assigning a literal empty string 
+empty_string = ""
+
+#another way - calling new on the String class
+empty_string = String.new
---
+++ ruby_basics/strings/quote_usage.rb
@@ -0,0 +1,10 @@
+#ugly.
+puts 'It\'s now 12 o\'clock.'
+
+#pretty!
+puts "It's now 12 o'clock."
+
+#note that single quotes do not allow for escaped characters.
+
+#nuclear option which allows for printing everything
+puts %Q("It's now 12 o'clock" )
