0492f74483a03a3a360070e0b2bd0c2017be8420
Matt Wood <w.matthew.wood@gmail.com>
2021-08-18 23:36:53 +0000

    Made some changes to the personal project - compound.rb (compound interest calculator)

--- personal_projects/compound.rb
+++ personal_projects/compound.rb
@@ -1,4 +1,6 @@
 puts "Welcome to the UltraFutures(TM) $$$|FutureMoney Money Maker Calculator|$$$"
+puts "What year do you want to start investing?"
+my_date = gets.chomp.to_i
 puts "How much money are you starting your investment strategy with?"
 my_starting_amount = gets.chomp.to_i
 puts "How many years are you planning to calculate for?"
@@ -10,17 +12,17 @@ puts "How much money do you want to contribute per year?"
 my_contributions = gets.chomp.to_i
 puts "Thank you... calculating..."
 
-def compound_interest(starting_amount, years, interest, contributions)
+def compound_interest(starting_amount, years, interest, contributions, date)
   running_total = starting_amount
-  date = 2020
   puts "#{years} years interest calculation with: \n#{starting_amount} initial investment \n#{(interest * 100 - 100).floor(2)}% interest rate \n$#{contributions} annual contributions"
-  years.times do
+  while years >= 0 do
     puts date.to_s + ": " + running_total.to_s 
     running_total = (running_total * interest + contributions).floor(2)
     date += 1
+    years -= 1
   end
 end
 
 #compound_interest(45000, 10, 1.10, 0)
-compound_interest(my_starting_amount, my_years, my_interest, my_contributions)
+compound_interest(my_starting_amount, my_years, my_interest, my_contributions, my_date)
 puts "Thank you for using the UltraFutures(TM) $$$|FutureMoney Money Maker Calculator|$$$"
