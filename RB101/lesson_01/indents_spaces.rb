ae6a542b3a718bfb189e91cbc5749fdd79aa755c
Matt Wood <w.matthew.wood@gmail.com>
2021-09-07 01:14:22 -0400

    RB101 lesson_01 added

---
+++ RB101/lesson_01/indents_spaces.rb
@@ -0,0 +1,6 @@
+this is a test
+  this is a tab
+  this is another tab
+  looks like two spaces to me.
+done.
+    
---
+++ RB101/lesson_01/ruby_formatting.rb
@@ -0,0 +1,40 @@
+# Naming a file
+
+this_is_a_snake_cased_filename.rb
+
+
+# Assigning a variable
+
+forty_two = 42
+once_again = 'we use snake_case_style_formatting for the variable names'
+
+
+# Defining a method
+
+def this_is_a_snake_cased_method
+  # do stuff
+end
+
+
+# Constant declaration - constants are denoted in ALL CAPS
+
+FOUR = 'four'
+FIVE = 5
+
+
+# Multi-live vs single line - if it can fit on a single line use {}
+
+[1, 2, 3.].each do |i|
+  # do stuff
+end
+
+[1, 2, 3].each { |i| # do stuff }
+
+
+# Class naming - use CamelCase 
+
+class MyFirstClass
+end
+
+class MySecondClass
+end
