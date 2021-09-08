dcbad69b72a62e6801e91195c6b728302ff0955f
Matt Wood <w.matthew.wood@gmail.com>
2021-09-08 19:00:47 +0000

    Added notes regarding pseudocode conventions.

---
+++ RB101/lesson_02/pseudocode_notes.rb
@@ -0,0 +1,8 @@
+START   start of the program
+SET     sets a variable we can use later
+GET     retrieve input from user
+PRINT   displays output to user
+READ    retrieve value from variable
+IF ELSE show conditional branches of logic
+WHILE   show looping logic
+END     end the program
