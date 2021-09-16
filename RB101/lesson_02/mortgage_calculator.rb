a01c16a7995036f7778f8676d04642c66a90a7aa
Matt Wood <w.matthew.wood@gmail.com>
2021-09-16 21:38:55 +0000

    Updated mortgage calculator - fixed rubocop errors.

--- RB101/lesson_02/mortgage_calculator.rb
+++ RB101/lesson_02/mortgage_calculator.rb
@@ -8,29 +8,28 @@ prompt('-----------------------------------')
 loop do
   prompt('How much money are you looking to take out as a loan?')
   loan_amount = Kernel.gets().chomp().to_f
+
   prompt('What is the Annual Percentage Rate (APR)?')
+  prompt("(Example: 5 for 5% or 2.5 for 2.5%)")
   apr = Kernel.gets().chomp().to_f
+
   prompt('How many years do you plan to take to pay back the loan?')
   prompt('(Please enter the loan duration in years.')
   loan_years = Kernel.gets().chomp().to_f
-
-  # prompt(loan_amount)
-  # prompt(loan_duration)
-  # prompt("% #{apr}")
-
+  
   monthly_rate = apr / 12 * 0.01
   loan_months = loan_years * 12
   monthly_payment = loan_amount *
                     (monthly_rate /
                     (1 - (1 + monthly_rate)**(loan_months * -1)))
   total_payment = loan_months.to_i * monthly_payment.to_i
-  total_interest = total_payment.to_i - loan_amount.to_i                  
+  total_interest = total_payment.to_i - loan_amount.to_i
 
   prompt("Your loan's duration will be #{loan_months.to_i} months.")
   prompt("Your monthly payments will be: $#{format('%.2f', monthly_payment)}.")
   prompt("Your total amount paid will be: $#{total_payment}.")
   prompt("Your amount of interest paid will be: $#{total_interest}.")
-  prompt("---------------------------------------------------------"
+  prompt("---------------------------------------------------------")
   prompt("Would you like to do another calculation? (Y to continue)")
   again = Kernel.gets().chomp().downcase()
 
