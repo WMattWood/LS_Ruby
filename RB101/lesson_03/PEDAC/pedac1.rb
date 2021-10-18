ced69c5c6453ddb5fd12f4251293bff7a87b6505
Matt Wood <w.matthew.wood@gmail.com>
2021-10-18 17:10:35 +0000

    Updated PEDAC exercise.

--- RB101/lesson_03/PEDAC/pedac1.rb
+++ RB101/lesson_03/PEDAC/pedac1.rb
@@ -1,3 +1,15 @@
+# PROBLEM:
+
+# Given a string, write a method change_me which returns the same
+# string but with all the words in it that are palindromes uppercased.
+
+# change_me("We will meet at noon") == "We will meet at NOON"
+# change_me("No palindromes here") == "No palindromes here"
+# change_me("") == ""
+# change_me("I LOVE my mom and dad equally") == "I LOVE my MOM and DAD equally"
+
+# SOLUTION:
+#
 # input: string
 # output: new string (not the same object)
 # rules:
