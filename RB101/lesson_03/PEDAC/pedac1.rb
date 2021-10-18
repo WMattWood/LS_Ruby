1edbf3ce5acb6c99d697b8b29c31b8dd5da463a9
Matt Wood <w.matthew.wood@gmail.com>
2021-10-18 12:57:30 -0400

    Adding PEDAC exercise

---
+++ RB101/lesson_03/PEDAC/pedac1.rb
@@ -0,0 +1,10 @@
+# input: string
+# output: new string (not the same object)
+# rules:
+#     Explicit requirements:
+#       - all palindromes must be capitalized
+#       - palindromes are case sensitive (ie. 'Dad' is not a palindrome, 'dad' is.)
+
+#     Implicit requirements:
+#       - the returned string shouldn't be the same object.
+#       - if input is an empty string, the result should be an empty string.
