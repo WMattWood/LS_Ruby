fe365f316a8a63dc87975b26be284a9f730f809b
Matt Wood <w.matthew.wood@gmail.com>
2021-10-25 15:46:18 -0400

    Adding final code for pedac3.rb (the sum_even_num_row problem)

--- RB101/lesson_04/PEDAC/pedac3.rb
+++ RB101/lesson_04/PEDAC/pedac3.rb
@@ -1,23 +1,3 @@
-def sum_even_num_row(row_number)
-  rows = []
-  row_length = 1
-    #steps 2 & 3
-    loop do 
-      #rows << create_row( , row_number)
-      row_length += 1
-      break if rows.length == row_number
-    end
-  rows #to do: sum the final row
-end
-
-def sum_even_num_row_each(row_number)
-  rows = []
-  (1..row_number).each do |current_row_number| 
-    rows << create_row(start_integer, current_row_number)
-  end
-  rows #to do: sum the final row
-end
-
 # ALGORITHM (sum_even_num_row)
 # 1. Create an empty 'rows' array to contain all of the rows
 # 2. Create a 'row' array and add it to the overall 'rows' array
@@ -25,6 +5,52 @@ end
 #   - All the rows have been created when the length of the 'rows' array is equal to the input integer
 # 4. Sum the final row
 # 5. Return the sum of the final row
+
+# Additional logic....
+# Calculating the 'start integer':
+# Rule: First integer of row == last integer of preceding row + 2
+# Algorithm:
+# - get the last row of the rows array
+# - get the last integer of that row
+# - add two to that integer
+
+# 1st solution
+def sum_even_num_row(row_number)
+  # initialize 'rows' array and starting values for create_row method
+  rows = []
+  row_length = 1
+  start_integer = 2
+    #steps 2 & 3 - looped
+    loop do 
+      #call the create_row method and pass it initial values
+      rows << create_row(start_integer, row_length)
+      #increment values
+      row_length += 1
+      start_integer = rows.last.last + 2
+      #exit loop once the row length is equal to row_number argument
+      break if rows.length == row_number
+    end
+  # return the sum of the final row in array 'rows'
+  rows.last.sum #to do: sum the final row
+end
+
+# 2nd Solution
+def sum_even_num_row_each(row_number)
+  rows = []
+  start_integer = 2
+  (1..row_number).each do |current_row_number| 
+    rows << create_row(start_integer, current_row_number)
+    start_integer = rows.last.last + 2
+  end
+  rows.last.sum #to do: sum the final row
+end
+
+# ALGORITHM (create_row):
+# 1. Create an empty 'row' array to contain the integers
+# 2. Add the starting integer to the 'row' array
+# 3. Increment the starting integer by 2 to get the next integer in the sequence
+# 4. Repeat steps 2 & 3 until the array has reached the correct length.
+# 5. Return the 'row' array
 
 def create_row(start_integer, row_length)
   #create empty 'row' array
@@ -44,14 +70,6 @@ def create_row(start_integer, row_length)
   row
 end
 
-# ALGORITHM (create_row):
-# 1. Create an empty 'row' array to contain the integers
-# 2. Add the starting integer to the 'row' array
-# 3. Increment the starting integer by 2 to get the next integer in the sequence
-# 4. Repeat steps 2 & 3 until the array has reached the correct length.
-# 5. Return the 'row' array
-
-
 # TEST CASES
 # row number: 1 --> sum is 2
 # row number: 2 --> sum is 10
@@ -61,12 +79,14 @@ p sum_even_num_row(1) == 2  #true
 p sum_even_num_row(2) == 10 #true
 p sum_even_num_row(4) == 68 #true
 
+p sum_even_num_row_each(1) == 2  #true
+p sum_even_num_row_each(2) == 10 #true
+p sum_even_num_row_each(4) == 68 #true
+
 # start: 2, length: 1 --> [2]
 # start: 4, length: 2 --> [4, 6]
 # start: 8, length: 3 --> [8, 10, 12]
 
-# p create_row(2, 1) == [2]         #true
-# p create_row(4, 2) == [4, 6]      #true
-# p create_row(8, 3) == [8, 10, 12] #true
-
-p sum_even_num_row(4)
+p create_row(2, 1) == [2]         #true
+p create_row(4, 2) == [4, 6]      #true
+p create_row(8, 3) == [8, 10, 12] #true
