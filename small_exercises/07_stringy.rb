6e2d3c48cb01c21471d0afbe2e04791a061407cd
Matt Wood <w.matthew.wood@gmail.com>
2021-12-04 13:25:11 -0500

    Adding back debugged 07_stringy.rb

--- small_exercises/07_stringy.rb
+++ small_exercises/07_stringy.rb
@@ -1,29 +1,26 @@
-require 'pry-byebug'
-
-def stringy(num)
-  output = ""
-  num.times do
-    binding.pry
-    if output.split.to_a.last == '0' && output.length == 0
-      output << '1'
-    else
-      output << '0'
-    end
-  end
-  output
-end
-
 # def stringy(num)
 #   output = ""
 #   num.times do
-#     if output.end_with?('1') && output.length > 0
-#       output << '0'
-#     else
+#     if output.split('').to_a.last == '0' || output.length == 0
 #       output << '1'
+#     else
+#       output << '0'
 #     end
 #   end
 #   output
 # end
+
+def stringy(num)
+  output = ""
+  num.times do
+    if output.end_with?('1') && output.length > 0
+      output << '0'
+    else
+      output << '1'
+    end
+  end
+  output
+end
 
 puts stringy(6) == '101010'
 puts stringy(9) == '101010101'
