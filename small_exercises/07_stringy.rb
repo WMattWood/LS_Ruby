41df92ec93d0af66fd24033f76e515d166a0be6d
Matt Wood <w.matthew.wood@gmail.com>
2021-12-04 18:21:18 +0000

    The last thing worked.  Hell yeah.  Adding 07_stringy.rb for debugging on laptop.

--- small_exercises/07_stringy.rb
+++ small_exercises/07_stringy.rb
@@ -1,31 +1,31 @@
 require 'pry-byebug'
 
-# def stringy(num)
-#   output = ""
-#   num.times do
-#     if output.split.to_a.last == '0' && output.length == 0
-#       output << '1'
-#     else
-#       output << '0'
-#     end
-#   end
-#   output
-# end
-
 def stringy(num)
   output = ""
   num.times do
     binding.pry
-    if output.end_with?('1') && output.length > 0
-      output << '0'
-    else
+    if output.split.to_a.last == '0' && output.length == 0
       output << '1'
+    else
+      output << '0'
     end
   end
   output
 end
 
-puts stringy(6) #== '101010'
-puts stringy(9) #== '101010101'
-puts stringy(4) #== '1010'
-puts stringy(7) #== '1010101'
+# def stringy(num)
+#   output = ""
+#   num.times do
+#     if output.end_with?('1') && output.length > 0
+#       output << '0'
+#     else
+#       output << '1'
+#     end
+#   end
+#   output
+# end
+
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
