e3e52720f26402da862a7a3a35034b745b5eeb5f
Matt Wood <w.matthew.wood@gmail.com>
2021-12-09 04:21:51 +0000

    Adding twenty_one.rb after rubocop edits.

--- RB101/lesson_06/twenty_one.rb
+++ RB101/lesson_06/twenty_one.rb
@@ -1,7 +1,7 @@
-require 'pry-byebug'
+# require 'pry-byebug'
 
-SUITS = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', "Jack", "Queen", "King", "Ace"]
+SUITS = %w(Hearts Diamonds Clubs Spades)
+VALUES = %w(2 3 4 5 6 7 8 9 10 Jack Queen King Ace)
 WELCOME = "***** WELCOME TO THE CASINO *****"
 TABLE = "__________21 TABLE__________"
 
@@ -9,18 +9,12 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def shuffle_cards()
+def shuffle_cards
   VALUES.product(SUITS).shuffle
 end
 
 def format_hand(cards)
-  parsed = cards.map {|card| "#{card[0]} of #{card[1]}"}
-  # parsed.last.prepend('and ')
-  # if parsed.size > 2
-  #   parsed.join(', ')
-  # else
-  #   parsed.join(' ')
-  # end
+  parsed = cards.map { |card| "#{card[0]} of #{card[1]}" }
   parsed.join(', ')
 end
 
@@ -31,38 +25,31 @@ def show_cards(dealer, player)
   prompt "Player has: #{format_hand(player)}"
 end
 
-def deal(hand)
-  system 'clear'
-  puts "Dealing..."
-  sleep 1
-end
-
+# rubocop: disable Style/ConditionalAssignment
 def total(hand)
   # create array of card values
-  values = hand.map {|card| card[0]}
+  values = hand.map { |card| card[0] }
   total = 0
 
   # add up values
   values.each do |value|
     if value == 'Ace'
-      total += 11 
+      total += 11
     elsif value.to_i == 0
       total += 10
     else
       total += value.to_i
     end
   end
-  
+
   # correct based on number of aces if over 21
-  values.select{ |card| card == "Ace"}.count.times do
+  values.select { |card| card == "Ace" }.count.times do
     total -= 10 if total > 21
   end
 
   total
 end
-
-# test_hand = [['7', 'x'], ['Ace', 'x'], ['Ace', 'x'], ['Ace', 'x']]
-# puts total(test_hand)
+# rubocop: enable Style/ConditionalAssignment
 
 def compare(dealer, player)
   if total(dealer) >= total(player)
@@ -75,10 +62,16 @@ end
 # LOAD MESSAGE
 puts WELCOME
 sleep 2
+
 # MAIN LOOP
 loop do
-# 1. Initialize deck
-deck = shuffle_cards
+  # 1. Initialize deck
+  deck = shuffle_cards
+  system 'clear'
+  prompt "Shuffling..."
+  sleep 2
+  prompt "Dealing..."
+  sleep 2
 
   # DECK LOOP
   loop do
@@ -88,13 +81,14 @@ deck = shuffle_cards
     player_win = false
     dealer_win = false
 
-    2.times do dealer_hand << deck.pop end
-    2.times do player_hand << deck.pop end
+    2.times { dealer_hand << deck.pop }
+    2.times { player_hand << deck.pop }
     # 3. Player turn
     loop do
       # - Display hand
       show_cards(dealer_hand, player_hand)
       # - Hit or stay
+      prompt "Player currently has #{total(player_hand)}"
       prompt "Do you want to stay or hit? (Stay/Hit)"
       answer = gets.chomp.downcase
       # - Repeat until bust or 'stay'
@@ -121,17 +115,17 @@ deck = shuffle_cards
     end
 
     # 5. Dealer turn
-    if !dealer_win 
+    if !dealer_win
       loop do
         # - Display hand
         show_cards(dealer_hand, player_hand)
         # - Hit or stay
         # - Repeat until total >= 17
-        if total(dealer_hand) >= 17 
+        if total(dealer_hand) >= 17
           prompt "Dealer stays."
           sleep 1
           break
-        else 
+        else
           prompt "Dealer hits."
           dealer_hand << deck.pop
           sleep 1
@@ -152,10 +146,12 @@ deck = shuffle_cards
       prompt "Player wins."
     elsif dealer_win
       prompt "Dealer wins."
-    elsif total(player_hand) >= total(dealer_hand)
+    elsif total(player_hand) > total(dealer_hand)
       prompt "Player wins."
-    else
+    elsif total(dealer_hand) > total(player_hand)
       prompt "Dealer wins."
+    else
+      prompt "It's a tie."
     end
 
     # 8. Another hand?
@@ -169,27 +165,13 @@ deck = shuffle_cards
     end
   end
 
-#9. Reshuffle the deck?
-prompt "Would you like to reshuffle the deck and start again? (Y/N)"
-answer = gets.chomp.downcase
-if answer.chars.first == 'y' 
-  system 'clear'
-  prompt "Shuffling..." 
-  sleep 1
-  prompt "Dealing..." 
-  sleep 1
-else
-  system 'clear'
-  prompt "Thank you for playing 21."
-  sleep 1
-  break
+  # 9. Reshuffle the deck?
+  prompt "Would you like to reshuffle the deck and start again? (Y/N)"
+  answer = gets.chomp.downcase
+  if answer.chars.first != 'y'
+    system 'clear'
+    prompt "Thank you for playing 21."
+    sleep 1
+    break
+  end
 end
-
-end
-
-
-
-
-
-
-
