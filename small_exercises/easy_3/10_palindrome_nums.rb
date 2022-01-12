349681d573dfb953a0fc061ff9f609f35b0246c0
Matt Wood <w.matthew.wood@gmail.com>
2022-01-12 04:00:20 +0000

    Adding remainder of Easy 3 exercises

--- small_exercises/easy_3/10_palindrome_nums.rb
+++ small_exercises/easy_3/10_palindrome_nums.rb
@@ -11,4 +11,4 @@ end
 p palindromic_number?(34543) == true
 p palindromic_number?(123210) == false
 p palindromic_number?(22) == true
-p palindromic_number?(005) == true
+p palindromic_number?(5) == true
