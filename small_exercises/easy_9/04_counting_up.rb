4c324d02e970c0b82a3ea8f6278e196efd520179
Matt Wood <w.matthew.wood@gmail.com>
2022-02-09 20:21:34 -0500

    Adding easy_9 exercise #5

--- small_exercises/easy_9/04_counting_up.rb
+++ small_exercises/easy_9/04_counting_up.rb
@@ -1,9 +1,13 @@
 def sequence(num)
-
+  output = []
+  1.upto(num) do |number|
+    output << number
+  end
+  output
 end
 
 # Input: integer
 # Ouput: array of numbers from 1 to that number
-sequence(5) == [1, 2, 3, 4, 5]
-sequence(3) == [1, 2, 3]
-sequence(1) == [1]
+p sequence(5) == [1, 2, 3, 4, 5]
+p sequence(3) == [1, 2, 3]
+p sequence(1) == [1]
