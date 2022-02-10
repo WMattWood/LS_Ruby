0e68a9bdc857c757dffb6cb645ed729d115f1a13
Matt Wood <w.matthew.wood@gmail.com>
2022-02-10 00:12:13 -0500

    Final updates to easy_9 exercise 11

--- small_exercises/easy_9/11_group_anagrams.rb
+++ small_exercises/easy_9/11_group_anagrams.rb
@@ -1,6 +1,30 @@
-def anagrams(array)
+def anagrams(words)
+  output = {}
 
+  words.each do |word|
+    key = word.chars.sort.join
+    # words.each do |word|
+    #   if word.chars.sort.join == key
+    #     output.has_key?(key) && !output[key].include?(word) ? 
+    #     output[key].push(word) :
+    #     output[key] = [word]
+    #   end
+    # end
+    if output.has_key?(key)
+      output[key].push(word)
+    else
+      output[key] = [word]
+    end
+  end
+  
+  output.each do |key, value|
+    puts "#{output[key]}"
+  end
 end
+
+# def is_anagram?(word1, word2)
+#   word1.chars.sort == word2.chars.sort
+# end
 
 # Input: An array of strings
 # Output: prints arrays which are anagrams of each other
