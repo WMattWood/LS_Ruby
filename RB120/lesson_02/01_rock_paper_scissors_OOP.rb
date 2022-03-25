d5bd9f73e652b8c8d027d8a34a3bc960216a6772
Matt Wood <w.matthew.wood@gmail.com>
2022-03-25 00:02:04 -0400

    Adding Rock Paper Scissors OOP project from lesson 02

---
+++ RB120/lesson_02/01_rock_paper_scissors_OOP.rb
@@ -0,0 +1,79 @@
+# Write a textual description of the problem or exercise.
+# Extract the major nouns and verbs from the description.
+# Organize and associate the verbs with the nouns.
+# The nouns are the classes and the verbs are the behaviors or methods.
+
+
+# Rock, Paper, Scissors is a two-player game where each player chooses
+# one of three possible moves: rock, paper, or scissors. The chosen moves
+# will then be compared to see who wins, according to the following rules:
+
+  # - rock beats scissors
+  # - scissors beats paper
+  # - paper beats rock
+
+# If the players chose the same move, then it's a tie.
+
+
+# Nouns/Objects: player, move, rule
+# Verbs/Methods: choose, compare
+
+
+class Player
+  def initialize
+    # maybe a "name"? what about a "move"?
+  end
+
+  def choose
+
+  end
+end
+
+
+
+
+class Move
+  def initialize
+    # seems like we need something to keep track
+    # of the choice... a move object can be "paper", "rock" or "scissors"
+  end
+end
+
+
+
+
+class Rule
+  def initialize
+    # not sure what the "state" of a rule object should be
+  end
+end
+
+
+
+# not sure where "compare" goes yet
+def compare(move1, move2)
+
+end
+
+
+
+class RPSGame
+  attr_accessor :human, :computer
+
+  def initialize
+    @human = Player.new
+    @computer = Player.new
+  end
+
+  def play
+    display_welcome_message
+    human.choose
+    computer.choose
+    display_winner
+    display_goodbye_message
+  end
+end
+
+
+### Start the game ###
+RPSGame.new.play
