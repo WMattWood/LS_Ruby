996ad29c20ffd6879037948a6b0e665c4a090738
Matt Wood <w.matthew.wood@gmail.com>
2022-04-26 12:05:48 -0400

    Updated display_board method - renamed to display_board and clear_screen_and_display_board.  Optional clear screen logic removed from the display_board method.

--- RB120/lesson_05/OOP_ttt.rb
+++ RB120/lesson_05/OOP_ttt.rb
@@ -88,6 +88,10 @@ class TTTGame
     @computer = Player.new(COMPUTER_MARKER)
   end
 
+  def clear
+    system 'clear'
+  end
+
   def welcome_message
     puts "Welcome to Tic Tac Toe!"
     puts ""
@@ -97,8 +101,7 @@ class TTTGame
     puts "Thanks for playing Tic Tac Toe! Goodbye!"
   end
 
-  def display_board(clear=true)
-    system 'clear' if clear
+  def display_board
     puts "You're a #{human.marker}. Computer is a #{computer.marker}."
     puts ""
     puts "     |     |     "
@@ -113,6 +116,11 @@ class TTTGame
     puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}  "
     puts "     |     |     "
     puts ""
+  end
+
+  def clear_screen_and_display_board
+    clear
+    display_board
   end
 
   def human_moves
@@ -132,7 +140,7 @@ class TTTGame
   end
 
   def display_result
-    display_board
+    clear_screen_and_display_board
 
     case board.detect_winner
     when human.marker
@@ -157,11 +165,11 @@ class TTTGame
   end
 
   def play
-    system 'clear'
+    clear
     welcome_message
 
     loop do # begin_game
-      display_board(false)
+      display_board
 
       loop do # begin_round
         human_moves
@@ -170,14 +178,14 @@ class TTTGame
         computer_moves
         break if board.someone_won? || board.full?
         
-        display_board
+        clear_screen_and_display_board
       end
 
       display_result
 
       break unless play_again?
       board.reset
-      system 'clear'
+      clear
       puts "Let's play again!"
     end
 
