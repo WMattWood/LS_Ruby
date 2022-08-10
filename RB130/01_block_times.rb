40f03bba3d670ed8b72cb68bc4af7754f92df661
Matt Wood <w.matthew.wood@gmail.com>
2022-08-09 22:04:43 -0400

    Adding in a re-coding of the #times method, using yield keyword to pass in a block of code at execution time.

--- RB130/01_block_times.rb
+++ RB130/01_block_times.rb
@@ -1,12 +1,21 @@
-def times(numbah)
+# this method takes a number argument, and will yield to a block
+# because we don't define what happens inside the while loop, this
+# detail is left up to the method caller - they can decide what to
+# do with the counter.
+
+def times(number)
   counter = 0
-  while counter < numbah do
+  while counter < number do
     yield (counter)
     counter +=1
   end
 
-  numbah
+  number
 end
+
+# at execution, the block is passed in to the #times method at the
+# spot where the keyword yield is used, and the counter value is
+# passed in as the argument to the block.
 
 times(5) do |num|
   puts num
