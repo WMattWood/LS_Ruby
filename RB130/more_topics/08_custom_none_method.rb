0ad7edd610c756205891d13c62985698da517110
Matt Wood <w.matthew.wood@gmail.com>
2022-08-16 19:09:10 -0400

    Adding solution to 08_custom_none_method.rb exercise.

---
+++ RB130/more_topics/08_custom_none_method.rb
@@ -0,0 +1,20 @@
+def none?(arr)
+  counter = 0
+
+  while counter < arr.size
+    return false if yield(arr[counter])
+    counter += 1
+  end
+
+  true
+end
+
+
+# tests
+p none?([1, 3, 5, 6]) { |value| value.even? } == false
+p none?([1, 3, 5, 7]) { |value| value.even? } == true
+p none?([2, 4, 6, 8]) { |value| value.odd? } == true
+p none?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false
+p none?([1, 3, 5, 7]) { |value| true } == false
+p none?([1, 3, 5, 7]) { |value| false } == true
+p none?([]) { |value| true } == true
