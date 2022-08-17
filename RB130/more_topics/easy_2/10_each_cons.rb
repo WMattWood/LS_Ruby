6f0a4347496a53bbd1baff075dfc462805d08fdb
Matt Wood <w.matthew.wood@gmail.com>
2022-08-17 18:21:16 -0400

    Adding #each_cons exercise solutions.

---
+++ RB130/more_topics/easy_2/10_each_cons.rb
@@ -0,0 +1,42 @@
+def each_cons(array, num_of_cons)
+  array.each_with_index do |item, idx|
+    break if (idx + num_of_cons) > array.size
+    yield(*array[idx...idx + num_of_cons])
+  end
+  nil
+end
+
+# problem
+# input: an array of elements, and an integer specifying the group_size
+# side effect: elements from the array are passed to the yielded block
+
+# tests
+hash = {}
+each_cons([1, 3, 6, 10], 1) do |value|
+  hash[value] = true
+end
+p hash == { 1 => true, 3 => true, 6 => true, 10 => true }
+
+hash = {}
+each_cons([1, 3, 6, 10], 2) do |value1, value2|
+  hash[value1] = value2
+end
+p hash == { 1 => 3, 3 => 6, 6 => 10 }
+
+hash = {}
+each_cons([1, 3, 6, 10], 3) do |value1, *values|
+  hash[value1] = values
+end
+p hash == { 1 => [3, 6], 3 => [6, 10] }
+
+hash = {}
+each_cons([1, 3, 6, 10], 4) do |value1, *values|
+  hash[value1] = values
+end
+p hash == { 1 => [3, 6, 10] }
+
+hash = {}
+each_cons([1, 3, 6, 10], 5) do |value1, *values|
+  hash[value1] = values
+end
+p hash == {}
