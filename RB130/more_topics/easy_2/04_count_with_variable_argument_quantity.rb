a3356af8f0aefd8895d4e1e54e2cb54bf7635e67
Matt Wood <w.matthew.wood@gmail.com>
2022-08-17 12:54:22 -0400

    Adding 04_count.rb exercises solution - this one demonstrates the possibility of designing a method which takes a variable number of arguments.

---
+++ RB130/more_topics/easy_2/04_count_with_variable_argument_quantity.rb
@@ -0,0 +1,14 @@
+#demonstrates passing in a variable quantity of args
+
+def count(*items)
+  true_count = 0
+  items.each { |item| true_count += 1 if yield(item) }
+  true_count
+end
+
+p count(1, 3, 6) { |value| value.odd? } == 2
+p count(1, 3, 6) { |value| value.even? } == 1
+p count(1, 3, 6) { |value| value > 6 } == 0
+p count(1, 3, 6) { |value| true } == 3
+p count() { |value| true } == 0
+p count(1, 3, 6) { |value| value - 6 } == 3
