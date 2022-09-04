2012dac5e488fc8af6b7af42001659718d33bd0a
Matt Wood <w.matthew.wood@gmail.com>
2022-09-04 14:15:56 -0400

    Adding solution to Diamond challenge.

---
+++ RB130/challenges/diamond.rb
@@ -0,0 +1,67 @@
+class Diamond
+  LETTERS = ('A'..'Z').to_a
+
+  class << self
+
+    def make_diamond(letter)
+      if letter == 'A'
+        'A' + "\n"
+      else
+        self.first_line(letter) +
+        self.middle_lines(letter) +
+        self.last_line(letter)
+      end
+    end
+    
+    private 
+    
+    def first_line(letter)
+      index = LETTERS.index(letter)
+      whitespace = " " * (index)
+      whitespace + 'A' + whitespace + "\n"
+    end
+
+    alias_method :last_line, :first_line
+
+    def middle_lines(letter)
+      index = LETTERS.index(letter) - 1
+      output = []
+      self.mirror_step(0, index) do |x, y|
+        letter = LETTERS[x + 1]
+        output << ((' ' * y) + letter + (' ' * (x * 2 + 1) ) + letter + (' ' * y))
+      end
+      output.flatten.join("\n") + "\n"
+    end
+
+    def mirror_step(start, finish)
+      a = start
+      b = finish
+      start.upto(finish) do
+        yield(a, b)
+        a += 1
+        b -= 1
+      end
+  
+      a = finish
+      b = start
+      (finish - 1).downto(0) do
+        a -= 1
+        b += 1
+        yield(a, b)
+      end
+    end
+
+    def oddstep(n)
+      n * 2 + 1
+    end
+
+  end
+  
+  
+
+
+
+end
+
+bof = Diamond.make_diamond('E')
+puts bof
